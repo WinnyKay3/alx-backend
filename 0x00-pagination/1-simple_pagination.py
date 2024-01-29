550831371627cdec69d3e3681d82b28b4536a635
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-29 18:17:15 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -29,16 +29,14 @@ class Server:
         """
         self.__dataset = None
 
-                                                                                            def dataset(self) -> List[List]:
-        """Cached dataset
-                                                                                                """
-                                                                                                if self.__dataset is None:
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
             with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)                                                                  dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
-                                                                                                return self.__data
-
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+                self.__dataset = dataset[1:]
+        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Retrieves a page of data.
