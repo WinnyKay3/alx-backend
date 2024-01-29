af4ff4514257f29675f7adcafa84699ba1767bfd
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-29 18:29:07 +0000

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-index_range that takes two interger args,page 
+index_range that takes two interger args,page
 and size
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     page numbers are one indexed first page is page one
     """
